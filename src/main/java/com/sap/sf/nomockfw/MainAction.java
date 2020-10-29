b5585a6d5bab58fe33a613d9854e59cbce7464f9
Arjun Vinod <arjunvinod3@gmail.com>
2020-10-29 11:15:44 +0530

    Refactored code Changed target file path to match source file path but to src/test folder

--- src/main/java/com/sap/sf/nomockfw/MainAction.java
+++ src/main/java/com/sap/sf/nomockfw/MainAction.java
@@ -1,6 +1,5 @@
 package com.sap.sf.nomockfw;
 
-import com.github.javaparser.JavaParser;
 import com.github.javaparser.Position;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
@@ -14,81 +13,72 @@ import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.ThisExpr;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
-import com.github.javaparser.ast.stmt.ReturnStmt;
-import com.github.javaparser.ast.stmt.TryStmt;
 import com.github.javaparser.ast.type.ReferenceType;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.successfactors.tool.codegen.common.utils.CodeGenUtil;
 import com.successfactors.tool.codegen.common.utils.FileUtil;
-import com.successfactors.tool.codegen.handlers.java.JavaHelper;
 import org.apache.commons.collections.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 public class MainAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         try {
-            generateStub(anActionEvent);
+            VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(anActionEvent.getData(LangDataKeys.EDITOR).getDocument());
+            File sourceFile = new File(virtualFile.getPath());
+            generateStub(sourceFile);
         } catch (Exception exception) {
             exception.printStackTrace();
         }
 
     }
 
-    private void generateStub(AnActionEvent anActionEvent) throws Exception {
-        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(anActionEvent.getData(LangDataKeys.EDITOR).getDocument());
+    private void generateStub(File inFile) throws Exception {
+            CompilationUnit compilationUnit = StaticJavaParser.parse(inFile);
 
-        File file = new File(virtualFile.getPath());
-
-        parser(file);
-
-    }
-
-    private void parser(File inFile) throws Exception {
-            CompilationUnit unit = StaticJavaParser.parse(inFile);
-
+            //get public and protected methods
             VoidVisitor<List<MethodDeclaration>> methodNameCollector = new MethodNameCollector();
-            List<MethodDeclaration> methodNames = new ArrayList<>();
-            methodNameCollector.visit(unit, methodNames);
+            List<MethodDeclaration> methodsToOverride = new ArrayList<>();
+            methodNameCollector.visit(compilationUnit, methodsToOverride);
 
-            createJavaClass(unit, methodNames);
+            //generate the Java Stub class
+            createJavaStubClass(compilationUnit, methodsToOverride, inFile.getAbsolutePath());
     }
 
 
-    private void createJavaClass(CompilationUnit existingFile, List<MethodDeclaration> methodNames) throws IOException {
+    private void createJavaStubClass(CompilationUnit existingFileCU, List<MethodDeclaration> methodsToOverride, String inFilePath) throws IOException {
 
-        CompilationUnit cu = new CompilationUnit();
-        cu.setPackageDeclaration(existingFile.getPackageDeclaration().get());
+        CompilationUnit stubFileCU = new CompilationUnit();
+        stubFileCU.setPackageDeclaration(existingFileCU.getPackageDeclaration().get());
 
+        //get class name
         List<String> className = new ArrayList<String>();
         VoidVisitor<List<String>> classNameCollector = new ClassNameCollector();
-        classNameCollector.visit(existingFile, className);
-        ClassOrInterfaceDeclaration finalClass = cu.addClass(className.get(0)+"Stub");
+        classNameCollector.visit(existingFileCU, className);
+        ClassOrInterfaceDeclaration finalClass = stubFileCU.addClass(className.get(0)+"Stub");
 
-        cu.addImport("com.successfactors.unittest.dsl.api.mock.IInvocationHandler");
-        cu.addImport("com.successfactors.unittest.dsl.mock.DslMocker");
-        cu.addImport("com.successfactors.unittest.dsl.mock.MethodObjectGenerator");
-        cu.addImport("java.lang.reflect.Method");
+        //add imports
+        stubFileCU.addImport("com.successfactors.unittest.dsl.api.mock.IInvocationHandler");
+        stubFileCU.addImport("com.successfactors.unittest.dsl.mock.DslMocker");
+        stubFileCU.addImport("com.successfactors.unittest.dsl.mock.MethodObjectGenerator");
+        stubFileCU.addImport("java.lang.reflect.Method");
 
+        //add private field
         finalClass.addPrivateField("IInvocationHandler", "invocationHandler");
 
+        //add constructor
         finalClass.addConstructor(Modifier.Keyword.PUBLIC)
                 .addParameter("IInvocationHandler", "invocationHandler")
                 .setBody(new BlockStmt()
@@ -97,17 +87,19 @@ public class MainAction extends AnAction {
                                 new NameExpr("invocationHandler"),
                                 AssignExpr.Operator.ASSIGN))));
 
-        String pathname = "C:/Users/I513559/Desktop/_temp/" + className.get(0) + "Stub.java";
-        File file = new File(pathname);
-        FileUtil.createNewFile(file);
-        FileUtil.writeToFile(file, Arrays.asList(cu.toString()));
+        //write to file
+        String targetPath = inFilePath.replace("src\\main", "src\\test");
+        targetPath = targetPath.replace(className.get(0), className.get(0)+"Stub");
+        File targetFile = new File(targetPath);
+        FileUtil.createNewFile(targetFile);
+        FileUtil.writeToFile(targetFile, Arrays.asList(stubFileCU.toString()));
 
-        //methods
-        for (MethodDeclaration methodName :
-                methodNames) {
-            int lineNumber = ((Position)getClassByName(file).getEnd().get()).line - 1;
-            String method1 = "\n\t@Override\n" +
-                    "\t"+ methodName.getDeclarationAsString() +" {\n" +
+        //add methods by overriding the existing public and protected methods
+        for (MethodDeclaration methodToOverride :
+                methodsToOverride) {
+            int methodPosition = ((Position)getClassByName(targetFile).getEnd().get()).line - 1;
+            String methodString = "\n\t@Override\n" +
+                    "\t"+ methodToOverride.getDeclarationAsString() +" {\n" +
                     "\t\ttry {\n" +
                     "\t\t\tMethod me = (new MethodObjectGenerator() {\n" +
                     "\t\t\t}).getMethod();\n" +
@@ -115,27 +107,27 @@ public class MainAction extends AnAction {
                     "\t\t\tif (!invocationHandler.isInvokeRealMethod(returnObject)) {\n" +
                     "\t\t\t\treturn (String) returnObject;\n" +
                     "\t\t\t}\n\t\t} ";
-            if (!CollectionUtils.isEmpty(methodName.getThrownExceptions())) {
+            if (!CollectionUtils.isEmpty(methodToOverride.getThrownExceptions())) {
                 for (ReferenceType referenceType:
-                     methodName.getThrownExceptions()) {
-                    method1 = method1 + "catch ("+ referenceType.asString() +" ex) {\n" +
+                     methodToOverride.getThrownExceptions()) {
+                    methodString = methodString + "catch ("+ referenceType.asString() +" ex) {\n" +
                             "\t\t\tthrow ex;\n" + "\t\t}\n\t\t";
                 }
             }
-            method1 = method1 + "catch (Throwable ex) {\n" +
+            methodString = methodString + "catch (Throwable ex) {\n" +
                     "\t\t\tinvocationHandler.handleUnexpectedException(ex);\n" +
                     "\t\t}\n" +
-                    "\t\treturn super."+methodName.getNameAsString()+"(";
+                    "\t\treturn super."+methodToOverride.getNameAsString()+"(";
             for (Parameter parameter :
-                    methodName.getParameters()) {
-                method1 = method1 + parameter.getNameAsString();
+                    methodToOverride.getParameters()) {
+                methodString = methodString + parameter.getNameAsString();
             }
-            method1 = method1 + ");\n\t}";
-            FileUtil.updateFileContent(pathname, method1, lineNumber);
+            methodString = methodString + ");\n\t}";
+            FileUtil.updateFileContent(targetPath, methodString, methodPosition);
         }
     }
 
-    protected ClassOrInterfaceDeclaration getClassByName(File file) throws FileNotFoundException {
+    private ClassOrInterfaceDeclaration getClassByName(File file) throws FileNotFoundException {
         String baseName = CodeGenUtil.getBaseName(file.getName());
         ClassOrInterfaceDeclaration classDeclaration = null;
         CompilationUnit cu = StaticJavaParser.parse(file);
